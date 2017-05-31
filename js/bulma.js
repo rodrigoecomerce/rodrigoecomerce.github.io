c5a1ef430f485d088565335a9948b27f465277ad
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-05-31 10:49:00 -0300

    Update bulma.js

--- js/bulma.js
+++ js/bulma.js
@@ -1,6 +1,6 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
+$( document ).ready(function() {
     var burger = document.querySelector('.nav-toggle');
     var menu = document.querySelector('.nav-menu');
     burger.addEventListener('click', function() {
@@ -15,4 +15,4 @@
     console.log(resp);
   });
     
-})();
+});
