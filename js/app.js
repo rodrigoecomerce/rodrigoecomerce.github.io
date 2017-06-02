c0f3edd16bca49b502b68b9ec5005b939fe6b9da
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-02 14:56:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 var navbar1 = Vue.component('navbar1', {
   template: `<div class="hero-head hero-head1">
-    <nav class="nav nav1">
 <div class="container">
-  <div class="nav-right nav-menu">
-            <span class="nav-item nav-item1">
+            <span class="nav-item nav-item1 is-right">
               <a href="tel:34993009650">
                 <span class="icon is-small">
                   <i class="fa fa-phone"></i>
@@ -11,9 +9,7 @@ var navbar1 = Vue.component('navbar1', {
                 <span>(34) 99300-9650</span>
               </a>
             </span>
-  </div>
 </div>
-      </nav>
 </div>`
 })
 
