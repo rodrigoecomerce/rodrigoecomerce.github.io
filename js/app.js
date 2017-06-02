6b66305546d69b020cd916068bd286ffe35be9c7
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-02 14:32:13 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,13 +15,43 @@ var navbar1 = Vue.component('navbar1', {
 </div>`
 })
 
+var navbar2 = Vue.component('navbar2', {
+  template: `<div class="hero-head nav2">
+			<header class="nav">
+				<div class="container">
+					<div class="nav-left">
+						<a class="nav-item" href="../index.html"> <img
+							src="https://rodrigoecomerce.github.io/images/bulma-white.png"
+							alt="Logo">
+						</a>
+					</div>
+					<span class="nav-toggle"> <span></span> <span></span> <span></span>
+					</span>
+					<div class="nav-right nav-menu">
+						<a class="nav-item is-active"> Home </a> <a class="nav-item">
+							Empresa </a> <a class="nav-item"> Serviços </a> <span
+							class="nav-item"> <a
+							class="button is-info is-outlined is-inverted"> <span
+								class="icon"> <i class="fa fa-envelope-o"></i>
+							</span> <span>Contato</span>
+						</a>
+						</span>
+					</div>
+				</div>
+			</header>
+		</div>
+`
+})
+
+		
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
   },
   components: {
-  navbar1
+  navbar1,
+    navbar2
   }
 })
 
