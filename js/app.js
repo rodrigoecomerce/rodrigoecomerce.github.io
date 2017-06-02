2c145f5a2ec9c1e8f9f39b0357967876365c2793
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-02 14:22:22 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
+var navbar1 = Vue.component('navbar1', {
+  template: `<div class="hero-head hero-head1">
+    <nav class="nav nav1">
+  <div class="nav-right nav-menu">
+            <span class="nav-item nav-item1">
+              <a href="tel:34993009650">
+                <span class="icon is-small">
+                  <i class="fa fa-phone"></i>
+                </span>
+                <span>(34) 99300-9650</span>
+              </a>
+            </span>
+  </div>
+      </nav>
+</div>`
+})
 
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  },
+  components: {
+  navbar1
   }
 })
+
