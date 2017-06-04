a106d8f55fe13471eecaf7688c6ae83823e3088a
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-04 16:19:22 -0300

    Update vueapp.js

--- js/vueapp.js
+++ js/vueapp.js
@@ -1,20 +1,9 @@
-// Creating a new Vue instance and pass in an options object.
-var demo = new Vue({
+import Vue from 'vue'  
+import VueRouter from 'vue-router' // << aqui  
 
-    // A DOM element to mount our view model.
-    el: '#app',
+Vue.use(VueRouter) // << e aqui
 
-    // This is the model.
-    // Define properties and give them initial values.
-    data: {
-        active: 'home'
-    },
-
-    // Functions we will be using.
-    methods: {
-        makeActive: function(item){
-            // When a model is changed, the view will be automatically updated.
-            this.active = item;
-        }
-    }
-});
+new Vue({  
+  el: 'body',
+  components: { App }
+})
