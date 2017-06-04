685e7589c1915648c46a32e0c4c1c600d8a4f1af
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-04 16:24:03 -0300

    Update vueapp.js

--- js/vueapp.js
+++ js/vueapp.js
@@ -1,9 +1,35 @@
 import Vue from 'vue'  
 import VueRouter from 'vue-router' // << aqui  
 
+var Mainpage = Vue.component('mainpage', {
+    template: "#mainpage",
+    methods: {
+             
+    }
+});
+
+var Contactpage = Vue.component('contactpage', {
+    template: "#contactpage",
+    methods: {
+             
+    }
+});
+
+
+const router = new VueRouter()
+
+router.map({  
+    '/': {
+        component: Mainpage
+    },
+    '/contact': {
+        component: Contactpage
+    },
+})
+
 Vue.use(VueRouter) // << e aqui
 
 new Vue({  
   el: 'body',
-  components: { App }
+  components: { Mainpage }
 })
