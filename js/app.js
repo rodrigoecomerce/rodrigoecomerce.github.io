5055006c9dcb35e04c37e28d98c3aa1ce46e410f
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-04 11:29:13 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,5 +7,18 @@
 $('.slider').slider();
   $('.contact').click(function(){
   $('.contato').removeClass( "hide" )
+    $('.page-body').addClass( "hide" )
   });
+  
+  
+  $('.submit').click(function(){
+  $('.contato').addClass( "hide" )
+    $('.page-body').removeClass( "hide" )
+  });
+  
+    $('.cancel').click(function(){
+  $('.contato').addClass( "hide" )
+    $('.page-body').removeClass( "hide" )
+  });
+  
 })(jQuery); // end of jQuery name space
