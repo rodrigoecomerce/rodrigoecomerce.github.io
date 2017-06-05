f2afb2a817ec1db8e20ceed506f038066d19e80d
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-04 22:22:21 -0300

    Update and rename vueapp.js to angularapp.js

---
+++ js/angularapp.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+var app = angular.module("myApp", []); 
+  app.controller("MainController", function($scope) {
+});
+    
+    
+  app.controller("ContatoController", function($scope) {
+});
+    app.config(function($routeProvider) {
+      $routeProvider
+        .when('/contato', {
+          templateUrl: 'contatopage.html',
+          controller: 'ContatoController'
+        })
+        .when('/', {
+          templateUrl: 'mainpage.html',
+          controller: 'MainController'
+        }).otherwise({
+          redirectTo: '/'
+        });
+    });
+})();
