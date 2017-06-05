fcc50f45157f439489e7309418ae03a58579555c
rodrigoecomerce <rodrigoecomerce@gmail.com>
2017-06-05 00:15:18 -0300

    Update angularapp.js

--- js/angularapp.js
+++ js/angularapp.js
@@ -1,6 +1,19 @@
 (function() {
   'use strict';
 var app = angular.module("myApp", ["ngRoute"]); 
+  
+  app.run(function($rootScope, $route, $routeParams, $location) {
+      
+      $rootScope.$on('$routeChangeStart',function(evt,next,current){
+        console.log('Nome do Evento:'+evt.name);
+        console.log('Próxima Rota:'+ angular.toJson(next));
+        console.log('Rota Atual:'+ angular.toJson(current));
+      });
+
+      $rootScope.$route = $route;
+      $rootScope.$location = $location;
+      $rootScope.$routeParams = $routeParams;
+});
   app.controller("MainController", function($scope) {
 });
     
